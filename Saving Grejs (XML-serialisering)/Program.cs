f692017d8ce745d682fa3e061f51335b7fc78662
Zvampish <nornostvold02@gmail.com>
2020-09-23 10:10:46 +0200

    Initial commit

---
+++ Saving Grejs (XML-serialisering)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Saving_Grejs__XML_serialisering_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
